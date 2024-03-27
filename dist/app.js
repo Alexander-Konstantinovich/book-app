15037cb99e2be673dacf558c11ef916804fac1ce
Alexander-Konstantinovich <shushpanov.ak@yandex.ru>
2024-03-28 00:07:24 +0300

    feat: structured the view.I created an App for routing.Added a hashchange handler. created an abstract view

--- dist/app.js
+++ dist/app.js
@@ -1,6 +1,20 @@
 (function () {
-	'use strict';
+    'use strict';
 
+    class App {
+        routes = [
+            {path:"", view: null}
+        ];
+        constructor(){
+            window.addEventListener('hashchange', this.route.bind(this));
+            this.route();
+        }
 
+        route(){
+            this.routes.find(r=>r.path == location.hash).view;
+        }
+    }
+
+    new App();
 
 })();
--- src/app.js
+++ src/app.js
@@ -1,2 +1,17 @@
 'use strict';
-import './app.css';
+
+class App {
+    routes = [
+        {path:"", view: null}
+    ];
+    constructor(){
+        window.addEventListener('hashchange', this.route.bind(this));
+        this.route();
+    }
+
+    route(){
+        const view = this.routes.find(r=>r.path == location.hash).view;
+    }
+}
+
+new App();
