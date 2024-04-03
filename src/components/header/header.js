b0cebb67d7479dd910065640280d3bb05f1c8d36
Alexander-Konstantinovich <shushpanov.ak@yandex.ru>
2024-04-03 11:30:54 +0300

    feat: added the first component header in render

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -9,5 +9,11 @@ export class Header extends DivComponent {
     render(){
         this.el.innerHTML = '';
         this.el.classList.add('header');
+        this.el.innerHTML = `
+        <div>
+            <img src="/static/logo.svg" alt= "Logotype" />
+        </div>
+        `;
+        return this.el;
     }
 }
